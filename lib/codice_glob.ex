4e5a6612b026e5b8482476cb23a3426f7553ebe5
Jason Pilz <jasonpilz@gmail.com>
2017-12-23 16:22:26 -0800

    First

---
+++ lib/codice_glob.ex
@@ -0,0 +1,18 @@
+defmodule CodiceGlob do
+  @moduledoc """
+  Documentation for CodiceGlob.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodiceGlob.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/codice_glob_test.exs
@@ -0,0 +1,8 @@
+defmodule CodiceGlobTest do
+  use ExUnit.Case
+  doctest CodiceGlob
+
+  test "greets the world" do
+    assert CodiceGlob.hello() == :world
+  end
+end
